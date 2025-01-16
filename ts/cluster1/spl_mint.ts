b962066bc956f3bff95f7720063d03650c639db7
spellsaif <this.saif.email@gmail.com>
2025-01-16 20:05:53 +0000

    lecture 1: creating ATA and minting some tokens

--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,6 +1,6 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
 import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import wallet from "../Turbin3-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -9,20 +9,36 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
-const token_decimals = 1_000_000n;
+const token_decimals = 10_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("435xeXtoLBpKPJF22KYBhztskmyhen1cJCbDNad47yQY");
 
 (async () => {
     try {
         // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+        const ata = await getOrCreateAssociatedTokenAccount(
+                            connection,
+                            keypair,
+                            mint,
+                            keypair.publicKey,
+                            false,
+                            commitment
+                        );
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+        // ATA(Associated Token Account Addr): 5cRdYCJRJn4ptjhyHFA8rTWVPwdqfzyyj5wtM6ckpU1G
 
         // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
+        const mintTx = await mintTo(
+                            connection,
+                            keypair,
+                            mint,
+                            ata.address,
+                            keypair.publicKey,
+                            1000n * token_decimals
+                    );
+        console.log(`Your mint txid: ${mintTx}`);
+        //tx: 3Hnt3eLcicXt5rxSGuyGzWXRu7mcePDmAMtQV8Dvx3eJyX4XzYVnDcXyKiAi2wpLMnrz7MXy6v7EYYKutdyWK89a
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }

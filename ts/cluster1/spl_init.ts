1f55b8c7fbf0075b92661dad6b59bafa52285c9a
spellsaif <this.saif.email@gmail.com>
2025-01-16 19:54:08 +0000

    lecture 1: creating mint account

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import wallet from "../Turbin3-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -11,8 +11,18 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
     try {
-        // Start here
-        // const mint = ???
+       //creating mint account
+       const mint = await createMint(
+                        connection,
+                        keypair,
+                        keypair.publicKey,
+                        null,
+                        4
+                    ) 
+    
+    console.log("Mint Account Address: ", mint.toBase58());
+
+    //Mint account address: 435xeXtoLBpKPJF22KYBhztskmyhen1cJCbDNad47yQY
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
